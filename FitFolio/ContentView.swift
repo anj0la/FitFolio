4d044e307dac6c50cabe9f495a9f60df4103b1ef
“anj0la” <anjola.aina@gmail.com>
2023-12-09 22:29:07 -0700

    created tabviews for nav along with sep views for each tabitem

--- FitFolio/ContentView.swift
+++ FitFolio/ContentView.swift
@@ -1,129 +1,68 @@
 //
-//  ContentView.swift
-//  FitFolio
+//  File: ContentView.swift
+//  App: FitFolio
+//  Author: Anjola aina
+//  Last Modified: Saturday, December 9th, 2023
 //
-//  Created by Anjola Aina on 2023-11-30.
-//  Last Modified on 2023-12-06.
+// Description:
+//
+// This Swift file defines the ContentView struct, which serves as the main content view for FitFolio. The ContentView includes a TabView to handle navigation between different screens within the application.
+//
+// Structure:
+// - ContentView: SwiftUI View struct responsible for the main content layout.
+//   - TabView: A SwiftUI container view that manages multiple child views, allowing users to navigate between them using tabs.
+//
+// Usage:
+// - ContentView is typically set as the root view in the application's main file (e.g., App.swift).
+// - Inside the TabView, different child views represent distinct screens or functionalities of the application.
+// - Each child view can be customized and extended as needed.
+
+// Example:
+//
+// struct ContentView: View {
+//      var body: some View {
+//          TabView {
+//              Child view 1
+//              Text("Screen 1")
+//              .tabItem {
+//              Image(systemName: "1.circle")
+//              Text("Screen 1")
+//              }
 //
 
 import SwiftUI
-import SwiftData
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
-    @State private var progress = 0.5
 
     var body: some View {
-        HStack {
-            VStack(alignment: .leading) {
-                generateHeader()
-                // Total kcals, fats, carbs, and protein the user has accumulated in respect to their target for the day
-                generateDailyTargetText()
-                generateDailyTargetProgressBars()
-                Divider()
-                
-                // Rows
-                ForEach(0..<4) { row in
-                    generateProgressBarRows()
+        TabView {
+            DashboardView()
+                .tabItem {
+                    Label("Dashboard", systemImage: "square.grid.2x2.fill")
                 }
-                generateDaysOfTheWeekText()
-                Spacer()
-            } // VStack
-            Spacer()
-        }.padding(.leading, 20) // HStack
+            JournalView()
+                .tabItem {
+                    Label("Journal", systemImage: "book.pages.fill")
+                }
+            AddView()
+                .tabItem {
+                    Label("Add", systemImage: "plus.circle.fill")
+                }
+            PathView()
+                .tabItem {
+                    Label("Path", systemImage: "point.topleft.down.to.point.bottomright.curvepath.fill")
+                }
+            MoreView()
+                .tabItem {
+                    Label("More", systemImage: "ellipsis")
+                }
+        }.tint(.black)
         
     } // body
     
-    /// Returns the string value of the current time of day.
-    ///
-    /// - Returns: The string value of the current time of day (i.e., morning, afternoon or evening).
-    private func getTimeOfDay() -> String {
-        let calendar = Calendar.current
-        let hour = calendar.component(.hour, from: Date())
-        switch hour {
-        case 0..<12:
-            return "Good morning"
-        case 12..<17:
-            return "Good afternoon"
-        default:
-            return "Good evening!"
-        } // switch
-    } // getTimeOfDay
-    
-    /// Returns the current day of the week as a string.
-    ///
-    /// - Returns: The current date of the week as a string.
-    private func getDay() -> String {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "EEE, MMM d"
-        return dateFormatter.string(from: Date())
-    } // getDay
-    
-    /// Returns a VStack containing the time of day and the current day.
-    ///
-    /// - Returns: VStack containing information about the current day.
-    private func generateHeader() -> some View {
-        VStack(alignment: .leading) {
-            Text(getTimeOfDay())
-                .font(.subheadline)
-            Text(getDay())
-                .font(.title)
-            Text("Nutrition Overview")
-                .font(.title3).padding(.top, 0.01)
-        }
-    } // generateHeader
-    
-    /// Returns an HStack of the daily targets of calories, fat, carbs and protein.
-    ///
-    /// - Returns: HStack containing information about the daily target text.
-    private func generateDailyTargetText() -> some View {
-        HStack {
-            Text("2000/2300").font(.caption2).padding(.leading, 12)
-            Text("10/50").font(.caption2).padding(.leading, 40)
-            Text("50/100").font(.caption2).padding(.leading, 52)
-            Text("25/75").font(.caption2).padding(.leading, 52)
-        }.padding(.top, 2.5)
-    } // generateDailyTargetText
-    
-    private func generateDailyTargetProgressBars() -> some View {
-        HStack {
-            ForEach(0..<4) { _ in
-                ProgressView(value: progress)
-                    .progressViewStyle(LinearProgressViewStyle(tint: .black))
-                    .padding()
-                    .scaleEffect(1.25)
-            }
-        }.padding(.top, -15)
-    } // generateDailyTargetProgressBars
-     
-    private func generateProgressBarRows() -> some View {
-        HStack {
-            ForEach(0..<7) { _ in
-                ProgressView(value: progress)
-                    .progressViewStyle(LinearProgressViewStyle(tint: .black))
-                    .padding()
-                    .scaleEffect(1.5)
-                    .rotationEffect(.degrees(90))
-                }
-            }
-    } // generateProgressBarRows
-    
-    private func generateDaysOfTheWeekText() -> some View {
-        HStack {
-            Text("Mo").font(.caption).bold().padding(.leading, 15)
-            Text("Tu").font(.caption).bold().padding(.leading, 25)
-            Text("We").font(.caption).bold().padding(.leading, 30)
-            Text("Th").font(.caption).bold().padding(.leading, 30)
-            Text("Fr").font(.caption).bold().padding(.leading, 30)
-            Text("Sa").font(.caption).bold().padding(.leading, 30)
-            Text("Su").font(.caption).bold().padding(.leading, 30)
-        }
-    } // generateDaysOfTheWeekText
-  
+   
 } // ContentView
 
 #Preview {
     ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
 } // #Preview
---
+++ FitFolio/NavigationViews/MoreView.swift
@@ -0,0 +1,18 @@
+//
+//  MoreView.swift
+//  FitFolio
+//
+//  Created by Anjola Aina on 2023-12-09.
+//
+
+import SwiftUI
+
+struct MoreView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MoreView()
+}
