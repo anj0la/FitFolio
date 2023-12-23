e24432bf9ff3f020d45177e67dc70f9754c8bcc0
“anj0la” <anjola.aina@gmail.com>
2023-12-23 13:26:35 -0700

    added today section in journal.

--- FitFolio/NavigationViews/JournalView.swift
+++ FitFolio/NavigationViews/JournalView.swift
@@ -11,7 +11,51 @@ struct JournalView: View {
     @Binding var selectedText: String
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/).onAppear { selectedText = "Journal" }
+        HStack {
+            VStack(alignment: .leading) {
+                generateHeader()
+                Spacer()
+            } // VStack
+            Spacer()
+        }.padding(.leading, 20).onAppear {selectedText = "Journal"} // HStack
+    } // body
+    
+    /// Returns a VStack containing the time of day and the current day.
+    ///
+    /// - Returns: VStack containing information about the current day.
+    private func generateHeader() -> some View {
+        HStack {
+            Button(action: placeholder) {
+                Image(systemName: "chevron.left")
+                    .font(.callout).foregroundStyle(.black)
+            }
+            Text("TODAY")
+                .font(.title3).padding(.top, 0.01)
+            Button(action: placeholder) {
+                Image(systemName: "chevron.right")
+                    .font(.callout).foregroundStyle(.black)
+            }
+        }.frame(maxWidth: 358, alignment: .center) // temp fix
+    } // generateHeader
+    
+    private func generateOverviewCircles() -> some View {
+        HStack {
+            Button(action: placeholder) {
+                Image(systemName: "chevron.left")
+                    .font(.callout).foregroundStyle(.black)
+            }
+            Text("TODAY")
+                .font(.title3).padding(.top, 0.01)
+            Button(action: placeholder) {
+                Image(systemName: "chevron.right")
+                    .font(.callout).foregroundStyle(.black)
+            }
+        }
+    } // generateOverviewCircles
+    
+    ///  Currently a placeholder function, but represents an action of the buttons on the cards with graphs.
+    private func placeholder() -> Void {
+        print("The button was clicked.")
     }
 }
 
